68e157ee11e3ce8b4dbf053782e0b1cbfac67748
Ryan <ryan.hennou@gmail.com>
2022-12-11 19:59:16 +0100

    fixing folders missing v2.1

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if [ $UID -eq 0 ]; then
     echo "Execution du script d'installation"
-    mkdir VenvProjet/logs && chmod 777
+    mkdir VenvProjet/logs && chmod 777 VenvProjet/logs
     git clone https://github.com/rbsec/dnscan.git
     cd dnscan && pip install -r requirements.txt
     cd ../ && pip install -r requirements.txt
