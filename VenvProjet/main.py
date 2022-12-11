2eaf9e0633b1e186e54589cc1064df16d95069b6
Ryan <ryan.hennou@gmail.com>
2022-12-11 17:34:23 +0100

    UrlScan/Shodan working + Adding args + Verification TLDs

--- VenvProjet/main.py
+++ VenvProjet/main.py
@@ -1,4 +1,6 @@
+import argparse
 import json
+import re
 import sys
 
 from OsintToolBuilder import OsintToolBuilder
@@ -12,8 +14,17 @@ def get_active_tools():
 
 if __name__ == "__main__":
     # read config file and loop where "use = yes"
-    tools = get_active_tools()
-    url = sys.argv[1]
-    for tool in tools:
-        OsintToolBuilder.getTool(tool, {"url": url}).run()
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("--help", "-h", action="help", help="Affiche l'aide")
+    parser.add_argument("--domain", "-d", help="Inserer le domaine à analyser", required=True)
+    args = parser.parse_args()
     
+    with open("tlds.txt") as file:
+        lines = [line.rstrip() for line in file]
+        tld = "|".join(lines)
+        if(re.match("^(https|http|ftp|sftp):\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.(" + tld + ")$", args.domain) is not None):
+            tools = get_active_tools()
+            for tool in tools:
+                OsintToolBuilder.getTool(tool, {"url": args.domain}).run()
+        else:
+            print('Domaine non valide : Format : https://domain.tld')
