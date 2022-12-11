a034d259f23dce5d56f37eed0be3428c1a6517ed
Ryan <ryan.hennou@gmail.com>
2022-12-11 19:47:24 +0100

    Fixing install script

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
-git clone https://github.com/rbsec/dnscan.git
-cd dnscan && pip install -r requirements.txt
-cd ../ && pip install -r requirements.txt
-apt install theharvester
+#!/bin/bash
+
+if [ $UID -eq 0 ]; then
+    echo "Execution du script d'installation"
+    git clone https://github.com/rbsec/dnscan.git
+    cd dnscan && pip install -r requirements.txt
+    cd ../ && pip install -r requirements.txt
+    apt install theharvester
+else
+  echo "Le script doit être lancé en tant que root"
+fi
