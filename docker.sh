ed4a489b1a11ca671dc4da4ca2189f854c2a9d00
Ryan <ryan.hennou@gmail.com>
2022-12-16 12:17:40 +0100

    Fixing Docker script

--- docker.sh
+++ docker.sh
@@ -4,7 +4,7 @@ if [ $UID -eq 0 ]; then
     echo "Execution du script Docker !"
     apt install docker.io
     docker build -t autosint .
-    echo "Installation terminée ! Tapez : 'docker run autosint' pour que lancer AutOsint !"
+    echo "Installation terminée ! Tapez : 'sudo docker run autosint' pour que lancer AutOsint !"
 else
   echo "Le script doit être lancé en tant que root"
 fi
